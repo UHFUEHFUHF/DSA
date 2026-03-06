4bb57f767d969755726dd4c1dde488e5e0c66d45
Hitesh Dubey <reenadubey7000@gmail.com>
2026-03-07 00:03:38 +0530

    Time: 7 ms (6.66%) | Memory: 19.2 MB (94.81%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -4,7 +4,6 @@ class Solution:
         for num in nums:
             temp = 0
             while num != 0:
-                digit = num % 10
                 num = num // 10
                 temp += 1
             if temp % 2 == 0:
